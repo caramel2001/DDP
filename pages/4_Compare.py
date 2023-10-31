e054226a46f6a37652ec1361b80b6b0197221e16
Pratham <agarwalpratham2001@gmail.com>
2023-11-01 03:09:52 +0800

    Added more vis

--- pages/4_Compare.py
+++ pages/4_Compare.py
@@ -6,7 +6,18 @@ from config.config import settings
 import plotly.express as px
 import ast
 from search.graph import get_generalized_topics_multiple
-
+# Extracted using community detection algorithm and ChatGPT to identiy the labels of the communities
+RESEARCH_GROUPS = {
+    'Hardware' :[2,4,29,70,15,50,39,62,51],
+    'Cypersecurity' : [82,35,71,37,40,45,28,73],
+    'AI and Machine Learning' : [17,60,5,48,82,19,49,53,26,77,58,56,85,79,75,62,63],
+    'Data Analytics and Mining': [43,12,64,76,63,47,22],
+    'ComputerVision': [32,41,36,9,44,80,42,11,13,38,33,7,6,14,52,25,10],
+    'Human Computer Interaction': [57,18,59,54,1,84,20],
+    'Networks': [46,83,34,78,16,65,0,68,30,31],
+    'Distributed Computing': [21,66,14,3,77],
+    'Bioinformatics': [27,69,23],
+}
 st.set_page_config(page_title="CompareProfessor Profiles", layout="wide")
 st.title("Compare Professors")
 
@@ -15,8 +26,14 @@ prof_data = read_prof_data(settings['PROF_DATA'])
 def format_funct(id):
     return prof_data.iloc[id]['Full Name']
 options = st.multiselect("Select a Professors to compare",prof_data.index.to_list(),max_selections=3,format_func=format_funct)
+research_group = st.multiselect("Select Research Groups",list(RESEARCH_GROUPS.keys()),default=None)
+if research_group:
+    for i in research_group:
+        options.extend(RESEARCH_GROUPS[i])
+        options = list(set(options))
 profs = [prof_data.iloc[id] for id in options]
 # print(profs)
+
 
 
 dfs = []
@@ -61,7 +78,7 @@ if dfs:
         height=350,  # Set the height of the plot
         xaxis_title='Year',
         yaxis_title='Publication Count',
-        xaxis=dict(tickmode='array'),  # Set custom x-axis tick values
+        xaxis=dict(tickmode='array', tickvals=df['year']),  # Set custom x-axis tick values
         showlegend=True,  # Remove the legend
         plot_bgcolor='rgba(0,0,0,0)',  # Make the background transparent
         xaxis_tickangle=90,  # Rotate x-axis labels
